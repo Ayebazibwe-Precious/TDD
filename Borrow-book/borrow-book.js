3aad446e16b7b0641ccacc1be92a58e77c32c080
Ayebazibwe-Precious <ayebazibwemaryprecious@gmail.com>
2025-11-13 10:20:24 +0300

    new commits

--- Borrow-book/borrow-book.js
+++ Borrow-book/borrow-book.js
@@ -3,9 +3,18 @@ function borrowBook(book_title, available_books) {
     // Remove the book
     const index = available_books.indexOf(book_title);
     available_books.splice(index, 1);
-    return `You have borrowed '${book_title}'.`;
+
+    const message = `You have borrowed '${book_title}'.`;
+    const remaining = `Available books: [${available_books.join(", ")}]`;
+    console.log(`${message} ${remaining}`);
+
+    return message;
   } else {
-    return `Sorry, '${book_title}' is not available.`;
+    const message = `Sorry, '${book_title}' is not available.`;
+    console.log(`${message} Available books: [${available_books.join(", ")}]`);
+
+    return message;
   }
 }
+
 module.exports = borrowBook;
